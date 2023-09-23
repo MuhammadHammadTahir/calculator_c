2896d026ff3ca0e0d2210eecfbca492fbcae269f
Muhammad Hammad tahir <125272216+MuhammadHammadTahir@users.noreply.github.com>
2023-09-23 14:44:50 +0500

    completed

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,66 +1,149 @@
 import React from 'react'
 
-
 function Btn(props) {
 
-  const handleClick = () => {
-    if(props.text==='CE')
-    {
-      props.update('');
-      props.setResult('');
+
+  const sqWsign = () => {
+    if (props.pre.length === 1) {
+      props.update('Syntax Error');
     }
-    else
-    {
-      if(props.text==='C')
-      {
-        if (props.pre.length > 0)
-        {
+    else {
+      let i = 0;
+      for (; i < props.pre.length - 1; i++) {
+        if (props.pre[i] === '√') {
+          break;
+        }
+      }
+      if (i === 0) {
+        const sqnum = props.pre.slice(i + 1);
+        console.log(sqnum);
+        const sq = Math.pow(parseFloat(sqnum), 1 / 2);
+        props.update(sq + props.text);
+        console.log(sq);
+        props.setsqRoot(false);     
+      }                             
+      else {
+        const sqpre = props.pre.slice(0, i - 1);
+        const sqnum = props.pre.slice(i + 1);
+        const sq = Math.pow(parseFloat(sqnum), 1 / 2);
+        const solvesqwsign = parseFloat(sqpre) + sq;
+        try {
+          const evalResult = eval(solvesqwsign);
+          props.update(evalResult.toString() + props.text);
+        } catch (error) {
+          console.error("Invalid expression:", error);
+        }
+        props.setsqRoot(false);
+      }
+    }
+  }
+  const simple = () => {
+
+    if (props.pre.length === 1) {
+      props.update('Syntax Error');
+    }
+    else {
+      let i = 0;
+      for (; i < props.pre.length - 1; i++) {
+        if (props.pre[i] === '√') {
+          break;
+        }
+      }
+      if (i === 0) {
+        const sqnum = props.pre.slice(i + 1);
+        const sq = Math.pow(parseFloat(sqnum), 1 / 2);
+        props.update(sq);
+        props.setsqRoot(false);
+      }
+      else {
+        const sqpre = props.pre.slice(0, i - 1);
+        const sqnum = props.pre.slice(i + 1);
+        const sq = Math.pow(parseFloat(sqnum), 1 / 2);
+        const solvesqwsign = parseFloat(sqpre) + sq;
+        try {
+          const evalResult = eval(solvesqwsign);
+          props.update(evalResult.toString());
+        } catch (error) {
+          console.error("Invalid expression:", error);
+        }
+        props.setsqRoot(false);
+      }
+    }
+  }
+  const handleClick = () => {
+
+    if (props.text === 'CE') {
+      props.update('');
+    }
+    else {
+      if (props.text === 'C') {
+        if (props.pre.length > 0) {
           let newtext = props.pre.slice(0, -1);
           props.update(newtext);
-          props.setResult(newtext);
         }
-        else
-        {
+        else {
           props.update('');
-          props.setResult('');
         }
       }
       else if (props.text === '=') {
-        try {
-          const evalResult = eval(props.pre); 
-          props.update(evalResult.toString()); 
-          props.setResult(evalResult.toString());
-        } catch (error) {
-          console.error("Invalid expression:", error);
-          props.setResult('Invalid expression');
+        if (props.sqroot === true) {
+          simple();
         }
-      } 
-      else
-      {
-        if(props.text==='x^2')
-        {
-          props.update(props.pre + '^2');
+        else {
+          try {
+            const check = props.text.slice(-2, -1);
+            console.log(check);
+            const evalResult = eval(props.pre);
+            props.update(evalResult.toString());
+          } catch (error) {
+            console.error("Invalid expression:", error);
+          }
         }
-        else
-        {
-          if(props.text==='√')
-          {
+      }
+      else {
+        if (props.text === 'x^2') {
+          let operatorIndex = -1
+          for (let i = props.pre.length - 1; i >= 0; i--) {
+            if (props.pre[i] === '+' || props.pre[i] === '-' || props.pre[i] === '*' || props.pre[i] === '/') {
+              operatorIndex = i;
+              break;
+            }
+          }
+          if (operatorIndex === -1) {
+            const squareResult = Math.pow(props.pre, 2);
+            props.update(squareResult.toString());
+          }
+          else {
+            const substring = props.pre.slice(operatorIndex + 1);
+            const squareResult = Math.pow(substring, 2);
+            if (squareResult === 0) {
+              props.update("Syntax Error");
+            }
+            else {
+              const substringBeforeOperator = props.pre.slice(0, operatorIndex + 1);
+              props.update(substringBeforeOperator + squareResult.toString());
+            }
+          }
+        }
+        else {
+          if (props.text === '√') {
+            props.setsqRoot(true);
             props.update(props.pre + '√');
           }
-          else
-          {
-            if(props.text==='.')
-            {
+          else {
+            if (props.text === '.') {
               props.update(props.pre + '.');
             }
-            else
-            {
-              if(props.text==='÷'||props.text==='x'||props.text==='-'||props.text==='+')
-              {
-                // props.setOper(true);
+            else {
+              if (props.text === '/' || props.text === '*' || props.text === '-' || props.text === '+') {
+                if (props.sqroot === true) {
+                  sqWsign();
+                }
+                else {
+                  props.update(props.pre + props.text);
+                }
               }
-              else
-              {
+              else {
                 props.update(props.pre + props.text);
               }
             }
@@ -71,7 +154,7 @@ function Btn(props) {
   }
 
   const handleDisable = () => {
-    if (props.text === '÷' || props.text === 'x' || props.text === '-' || props.text === '+'||props.text === '=') {
+    if (props.text === '/' || props.text === '*' || props.text === '-' || props.text === '+' || props.text === '=') {
       if (props.pre.length === 0) {
         return true;
       } else {
@@ -85,7 +168,7 @@ function Btn(props) {
 
   return (
     <>
-    <button disabled={handleDisable()}className={`btn btn-${props.color} mx-2 my-1 `} type="submit" onClick={handleClick}>{props.text}</button>
+      <button disabled={handleDisable()} className={`btn btn-${props.color} mx-2 my-1 `} type="submit" onClick={handleClick}>{props.text}</button>
     </>
   )
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,44 +3,42 @@ import Btn from './Btn'
 function Buttons(props) {
 
     const [uptext, setUptext] = useState(props.write);
-    const [result, setResult] = useState(""); 
+    const [sqroot, setsqRoot] = useState(false); 
+    const [sign, setSign] = useState(false); 
     props.setText(uptext);
 
 
   return (
     <div className='btnbox'>
         <div className='btton'>
-            <Btn color='danger'     text='CE'      pre={uptext}     update={setUptext} setResult={setResult}/>
-            <Btn color='primary'    text='C'       pre={uptext}     update={setUptext} setResult={setResult}/>
-            <Btn color='info'       text='√'       pre={uptext}     update={setUptext} />
-            <Btn color='info'       text='÷'       pre={uptext}     update={setUptext} />
+            <Btn color='danger'     text='CE'      pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} />
+            <Btn color='primary'    text='C'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='info'       text='√'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} sign={sign} setSign={setSign}/>
+            <Btn color='info'       text='/'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} sign={sign} setSign={setSign}/>
         </div> 
         <div className='btton'>
-            <Btn color='secondary'  text='7'       pre={uptext}     update={setUptext} />
-            <Btn color='secondary'  text='8'       pre={uptext}     update={setUptext} />
-            <Btn color='secondary'  text='9'       pre={uptext}     update={setUptext} />
-            <Btn color='info'       text='x'       pre={uptext}     update={setUptext} />
+            <Btn color='secondary'  text='7'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary'  text='8'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary'  text='9'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='info'       text='*'       pre={uptext}     update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} sign={sign} setSign={setSign}/>
         </div>
         <div className='btton'>
-            <Btn color='secondary'  text='5'       pre={uptext}    update={setUptext}  />
-            <Btn color='secondary'  text='4'       pre={uptext}    update={setUptext}  />
-            <Btn color='secondary'  text='6'       pre={uptext}    update={setUptext}  />
-            <Btn color='info'       text='-'       pre={uptext}    update={setUptext}  />
+            <Btn color='secondary'  text='5'       pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary'  text='4'       pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary'  text='6'       pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='info'       text='-'       pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot} sign={sign} setSign={setSign}/>
         </div>
         <div className='btton'>
-            <Btn color='secondary' text='1'        pre={uptext}    update={setUptext}  />
-            <Btn color='secondary' text='2'        pre={uptext}    update={setUptext}  />
-            <Btn color='secondary' text='3'        pre={uptext}    update={setUptext}  />
-            <Btn color='info'      text='+'        pre={uptext}    update={setUptext}  />
+            <Btn color='secondary' text='1'        pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary' text='2'        pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='secondary' text='3'        pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot}/>
+            <Btn color='info'      text='+'        pre={uptext}    update={setUptext}  sqroot={sqroot} setsqRoot={setsqRoot} sign={sign} setSign={setSign}/>
         </div>
         <div className='btton'>
-            <Btn color='info'      text='x^2'      pre={uptext}    update={setUptext}  />
-            <Btn color='secondary' text='0'        pre={uptext}    update={setUptext} />
-            <Btn color='info'      text='.'        pre={uptext}    update={setUptext}  />
-            <Btn color='success'   text='='        pre={uptext}    update={setUptext}  setResult={setResult}/>
-        </div>
-        <div className='result'>
-            {result !== "" && <div className='result-text'>{result}</div>}
+            <Btn color='info'      text='x^2'      pre={uptext}    update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} />
+            <Btn color='secondary' text='0'        pre={uptext}    update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} />
+            <Btn color='info'      text='.'        pre={uptext}    update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} />
+            <Btn color='success'   text='='        pre={uptext}    update={setUptext} sqroot={sqroot} setsqRoot={setsqRoot} />
         </div>
     </div>
   )
